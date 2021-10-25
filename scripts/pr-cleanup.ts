b9eba8624ad7ab854895cfaa4e37fa76194f17f7
Charlton Roberts <charltonroberts@gmail.com>
2021-10-24 21:43:03 -0400

    Delete environment

--- scripts/pr-cleanup.ts
+++ scripts/pr-cleanup.ts
@@ -1,16 +1,17 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { Octokit } from '@octokit/core';
+import type { DeleteEnvironmentPayload, Repository } from '@octokit/graphql-schema'
 
 async function execute(){
   const octokit = github.getOctokit(process.env.GITHUB_TOKEN as string)
 
+  // Generate environment name from Pull Request number
   const prNumber = github.context.issue.number
   const environmentName = `pr-${prNumber}`
   console.log(`Removing environment ${environmentName}`)
 
-  // Does environment exist?
-  const environment = await octokit.graphql(`
+  // Get environment associated with this PR
+  const { repository } = await octokit.graphql<{ repository: Repository }>(`
     query GetEnvironment($repoName: String!, $repoOwner: String!, $environmentName: String!) { 
       repository(name: $repoName, owner: $repoOwner) { 
         environment(name: $environmentName){
@@ -25,7 +26,23 @@ async function execute(){
     environmentName
   });
 
-  console.log(JSON.stringify(environment))
+  // Environment does not exist, we're done
+  if (!repository?.environment?.name){
+    core.notice(`No environment "${environmentName}" found.`)
+    process.exit(0)
+  }
+
+  // Delete environment
+  await octokit.graphql<{ deleteEnvironment: DeleteEnvironmentPayload }>(`
+    mutation DeleteEnvironment($environmentId: ID!) { 
+      deleteEnvironment(id: $environmentId) { 
+        clientMutationId
+      }
+    }
+  `, {
+    environmentId: repository.environment.id
+  });
+  
   core.notice(`Removed environment ${environmentName}`)
 }
 
